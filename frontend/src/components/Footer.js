b22738ff370816ef58c43fadde1b9ad76e2df617
Chaitanya Kumar Chandra <chaitanyachandra2000@gmail.com>
2021-10-29 22:43:25 +0530

    Product Screen

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
     <footer>
-      <Container>
+      {/* <Container className="footer_bar">
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; ComfyHouse</Col>
+          <Col className="text-center py-3">Copyright &copy; ComfyHouse</Col>
         </Row>
-      </Container>
+      </Container> */}
+      <div className="footer_bar">
+        <Row style={{ margin: "0" }}>
+          <Col className="text-center py-3">Copyright &copy; ComfyHouse</Col>
+        </Row>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
