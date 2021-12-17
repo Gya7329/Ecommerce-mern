34b967bd586cf59bf0d518cd2daec0c3abc5a235
Gya7329 <gya7329@gmail.com>
2021-12-17 20:48:23 +0530

    final

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -40,14 +40,7 @@ const PaymentScreen = ({ history }) => {
               checked
               onChange={(e) => setPaymentMethod(e.target.value)}
             ></Form.Check>
-            <Form.Check
-              type='radio'
-              label='Stripe'
-              id='Stripe'
-              name='paymentMethod'
-              value='Stripe'
-              onChange={(e) => setPaymentMethod(e.target.value)}
-            ></Form.Check>
+         
           </Col>
           <Button type='submit' variant='primary'>
           Continue
